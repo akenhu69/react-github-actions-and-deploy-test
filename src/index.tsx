ba263419d976d5bf21967c6902ec7714a1bc1f91
Aken_Hu <aken_hu@mtelnet.com>
2023-03-30 11:30:55 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: "#00b96b",
+      },
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
